2ed6b8df26c3d02832115c1cc27acacb87642bd8
赵安家 <anjia0532@gmail.com>
2021-05-17 09:33:40 +0800

    first commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url        = 'https://dl.shuax.com/MouseInc2.10.21.7z'
+
+
+$installDir = "$toolsDir\mouseInc"
+
+$pp = Get-PackageParameters
+if ($pp.InstallDir) {
+    $installDir = $pp.InstallDir
+}
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $installDir
+  url           = $url
+  url64bit      = $url
+
+  softwareName  = 'mouse-inc*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+
+  # Checksums are now required as of 0.10.0.
+  # To determine checksums, you can get that from the original site if provided. 
+  # You can also use checksum.exe (choco install checksum) and use it 
+  # e.g. checksum -t sha256 -f path\to\file
+  checksum      = 'FDB6DC659DC583FC3C7DE911910DEFDAE80B2C593180B6039F36D348EF345BB5'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
